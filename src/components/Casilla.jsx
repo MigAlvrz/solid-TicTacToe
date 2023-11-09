00251c8bfbb52393871db31fded7b69dadf5e9f7
Miga <milual1988@gmail.com>
2023-11-09 10:25:08 +0100

    implementing the basic game logic plus a little styiling

--- src/components/Casilla.jsx
+++ src/components/Casilla.jsx
@@ -1,12 +1,16 @@
+import { createSignal } from 'solid-js';
 import './Casilla.css'
 
- function Casilla({id}) {
+ function Casilla({id, jugarTurno}) {
+    const [val, setVal] = createSignal();
+
+    const jugar = () => {
+        setVal(jugarTurno(id))
+    }
 
     return (
         <>
-            <div class="casilla">
-
-            </div>
+            <div class={`casilla ${val()}`} onClick={jugar}>{val()}</div>
         </>
     );
 }
--- src/components/Tablero.jsx
+++ src/components/Tablero.jsx
@@ -2,31 +2,68 @@ import { For, createSignal } from "solid-js";
 import Casilla from "./Casilla";
 import './Tablero.css'
 
-const [casillas, selectCasillas] = createSignal([]);
+const [casillas, setCasillas] = createSignal(Array(9).fill(null));
 const [isXTurn, setIsXTurn] = createSignal(false);
-const [resultado, setResultado] = createSignal();
+const [ganador, setGanador] = createSignal();
+const [gameOn, setGameOn] = createSignal(true);
 
-const resultadosPosibles = [
-    { 1: 1, 2: 2, 3: 3 },
-    { 1: 4, 2: 5, 3: 6 },
-    { 1: 7, 2: 8, 3: 9 },
-    { 1: 1, 2: 4, 3: 7 },
-    { 1: 2, 2: 5, 3: 8 },
-    { 1: 3, 2: 6, 3: 9 },
-    { 1: 1, 2: 5, 3: 9 },
-    { 1: 3, 2: 5, 3: 7 },
-];
+const resPosibles = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+  ];
+
+  const jugarTurno = (id) => {
+    if(!casillas()[id]) {
+        casillas()[id] = isXTurn() ? "X" : "O"
+        setIsXTurn(!isXTurn());
+        console.log(casillas());
+        calcGanador()
+        return casillas()[id]
+    }
+    return casillas()[id]
+  }
+
+  function nuevoJuego() {
+    setGameOn(false)
+    setCasillas(Array(9).fill(null));
+    setIsXTurn(true);
+    setGanador(null)
+    setGameOn(true)
+  }
+
+
+
+ function calcGanador()  {
+    resPosibles.map((e) => {
+        const [a, b, c] = e
+        if (
+            false
+        ) {}
+    })
+ }
 
  function Tablero() {
+  
+
 
     return (
         <>
             <h1 class="test">Tres en raya</h1>
-            <button>Nuevo juego</button>
+            <button onClick={nuevoJuego}>Nuevo juego</button>
             <div class="grid">
-                <For each={[0,1,2,3,4,5,6,7,8]}>
-                    {(id) => <Casilla id={id} />}
-                </For>
+                { gameOn() &&
+                    <For each={casillas()} >
+                        {
+                            (cas, i) => <Casilla jugarTurno={() => jugarTurno(i())} />
+                        }
+                    </For>
+                }
             </div>
         </>
     );
